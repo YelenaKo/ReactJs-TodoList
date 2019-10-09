0a3f8d4ba309a427cc789541ec9bc8c506be8b9c
Yelena <yelenawm@gmail.com>
2019-10-09 16:00:32 -0500

    Updates

---
+++ precache-manifest.aa2b51c4d6e9aebca35b230682bb805f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4934f6008fc8966203e3b3c19f2d0110",
+    "url": "/ReactJs-TodoList/index.html"
+  },
+  {
+    "revision": "4ba0e08a557c7bb6b7de",
+    "url": "/ReactJs-TodoList/static/css/main.e067b9af.chunk.css"
+  },
+  {
+    "revision": "d45e4e1281312e0db3fc",
+    "url": "/ReactJs-TodoList/static/js/2.31bce3e2.chunk.js"
+  },
+  {
+    "revision": "4ba0e08a557c7bb6b7de",
+    "url": "/ReactJs-TodoList/static/js/main.783d0ff5.chunk.js"
+  },
+  {
+    "revision": "f98b35efa7c76e89733c",
+    "url": "/ReactJs-TodoList/static/js/runtime-main.dc329526.js"
+  }
+]);
