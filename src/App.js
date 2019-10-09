684f2112bfd27735b4af2bfc3b5a1fec6fe3902a
Yelena <yelenawm@gmail.com>
2019-10-09 18:17:34 -0500

    route

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
         <div className = "App">
           <div className = 'container'>
             <Header />
-            <Route exact path="/" render={props => (
+            <Route exact path={process.env.PUBLIC_URL + '/'} render={props => (
               <React.Fragment>
                 <AddTodo addTodo={this.addTodo} />
                 <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} /> 
