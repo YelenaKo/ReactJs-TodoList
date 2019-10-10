f132d516de1885473c4951884ea5f6580116c2cb
Yelena <yelenawm@gmail.com>
2019-10-09 21:24:52 -0500

    router-v2.

--- src/App.js
+++ src/App.js
@@ -45,13 +45,13 @@ class App extends Component {
         <div className = "App">
           <div className = 'container'>
             <Header />
-              <Route exact path={process.env.PUBLIC_URL + '/'} render={props => (
+              <Route exact path={'/'} render={props => (
                 <React.Fragment>
                   <AddTodo addTodo={this.addTodo} />
                   <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} /> 
                 </React.Fragment>
               )} />
-              <Route path= {process.env.PUBLIC_URL + '/about'} component={ About } />
+              <Route path= {'/about'} component={ About } />
           </div>
         </div>
       </Router>
