a5a3a6f06583f47ec951c0d34279b13fa817a549
YelenaKo <37521732+YelenaKo@users.noreply.github.com>
2019-10-10 23:21:32 -0500

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter,  as Router, Route} from 'react-router-dom';
 import './App.scss';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
